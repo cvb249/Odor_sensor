23bc3a78655156b40132d9f65812e30a4fb0ef36
cvb249 <75022438+cvb249@users.noreply.github.com>
2020-11-28 16:02:14 +0900

    new project
    sampling for learning R packaging

---
+++ R/hello.R
@@ -0,0 +1,3 @@
+#' @tab = reading the data file
+sample <- read.csv("SEPTIC_TANK_2020.08.03-08.09.csv")
+sample
---
+++ R/tab-data.R
@@ -0,0 +1,14 @@
+#' Seoul city's septic tank odor sensor data
+#'
+#' Data from 'data.seoul.go.kr'
+#'
+#' @docType data
+#'
+#' @usage data(tab)
+#'
+#' @keywords datasets
+#'
+#' @references http://data.seoul.go.kr/dataList/OA-15966/S/1/datasetView.do
+#'
+#' @examples
+#' data(tab)
